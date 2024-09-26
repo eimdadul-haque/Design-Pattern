11af1d9ee20ec3d128fa11935b7b0a0f78a170f3
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-26 23:22:45 +0600

    non thread safe singleton

--- Creational Pattern/Singleton Pattern/Program.cs
+++ Creational Pattern/Singleton Pattern/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+//Non-Thread-Safe
+public class SingletonOne
+{
+    public static SingletonOne _singletonOne;
+    private SingletonOne() { }
+
+    public static SingletonOne Instance
+    {
+        get
+        {
+            if (_singletonOne == null)
+            {
+                _singletonOne = new SingletonOne();
+            }
+
+            return _singletonOne;
+        }
+    }
+}
